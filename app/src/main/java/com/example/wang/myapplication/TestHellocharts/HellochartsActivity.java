d370a8040b12b7fbcbcac97d0db756a347da94a2
t460pWjj <wjj99@qq.com>
2018-07-13 14:02:37 +0800

    添加hellocharts的一个折线图demo

---
+++ app/src/main/java/com/example/wang/myapplication/TestHellocharts/HellochartsActivity.java
@@ -0,0 +1,9 @@
+package com.example.wang.myapplication.TestHellocharts;
+
+/**
+ * Created by wjj on 2018/7/13 11:29
+ * E-Mail ：wjj99@qq.com
+ * 描述：
+ */
+public class HellochartsActivity {
+}
---
+++ app/src/main/java/com/example/wang/myapplication/testWords/FragmentTranscript.java
@@ -0,0 +1,70 @@
+package com.example.wang.myapplication.testWords;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.wang.myapplication.R;
+
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+
+/**
+ * 成绩单
+ * A simple {@link Fragment} subclass.
+ */
+
+public class FragmentTranscript extends BaseFragment {
+
+
+    private TextView mTextViewWord;
+
+
+    @Override
+    protected int setView() {
+        return R.layout.fragment_transcript;
+    }
+
+    @Override
+    protected void init(View view) {
+        mTextViewWord = view.findViewById(R.id.text_word);
+        mTextViewWord.setText("fragmentPre1");
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
+    }
+
+
+//此方法在Fragment中
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (hidden) {
+            //Fragment隐藏时调用
+            Toast.makeText(getActivity(), "隐藏fragment 1", Toast.LENGTH_SHORT).show();
+        } else {
+            //Fragment显示时调用
+            //mTextView.setText("显示fragmentPre" + num);
+        }
+
+    }
+
+
+    /**
+     * 从发布者那里得到eventbus传送过来的数据
+     * <p>
+     * 加上@Subscribe以防报错：its super classes have no public methods with the @Subscribe annotation
+     *
+     * @param event
+     */
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(MessageEvent event) {
+        mTextViewWord.setText("fragmentPre" + event.num);
+    }
+}
